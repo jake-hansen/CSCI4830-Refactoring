a74a6687eb9850a583dba4bb00e54472746fea40
Jake Hansen <jakedhansen@outlook.com>
2020-03-31 21:51:16 -0500

    Refactoring A (#1)
    * Add commonMethod
    * Add Graph super class
    * Fix common method type parameter
    - Refactor m1 and m2 to include common method

--- src/q1/extract_method/refactored/A.java
+++ src/q1/extract_method/refactored/A.java
@@ -4,35 +4,35 @@ import java.util.List;
 
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
+      commonMethod(nodes, p);
       // other implementation
       return null;
    }
 
    Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
+      commonMethod(edgeList, p);
       // other implementation
       return null;
    }
 
-   // TODO: Your answer
-   // ? extractedMethod(?) {
-   //
-   // }
-}
-
-class Node {
-   String name;
-
-   public boolean contains(String p) {
-      return name.contains(p);
+   <T extends Graph> void commonMethod(List<T> list, String p) {
+      for (T t : list) {
+         if (t.contains(p))
+            System.out.println(t);
+      }
    }
 }
 
-class Edge {
+class Graph {
    String name;
 
-   public boolean contains(String p) {
-       return name.contains(p);
-   }
+   public boolean contains(String p) { return name.contains(p); }
+}
+
+class Node extends Graph {
+
+}
+
+class Edge extends Graph {
+
 }
